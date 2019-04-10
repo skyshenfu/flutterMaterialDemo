e93028f03dc3a1bcc74d3fb219c8f6585013332f
zty <965819494@qq.com>
2019-04-10 15:02:09 +0800

    完成底部导航的demo

---
+++ lib/index_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_material_demo/pages/bottomdemo/bottom_index_page.dart';
+
+
+ ///总索引页面，根据不同按钮跳转到不同的索引Page
+class IndexPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new RaisedButton(onPressed: (){
+              Navigator.push(context, new MaterialPageRoute(builder: (context){
+                return new BottomIndexPage();
+              }));
+            }, child: Text("底部导航"),)
+          ],
+        ),
+      )
+    );
+  }
+  
+}
